6a35677d06e2dde6d9a5e2b632df3ef11743760a
ramirozap <ramaxzapata@gmail.com>
2016-01-03 10:52:54 -0300

    param pam pam solved

---
+++ param-pam-pam.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+
+app.put('/message/:ID', function(req, res) {
+  res.end(require('crypto')
+   .createHash('sha1')
+   .update(new Date().toDateString() + req.params.ID)
+   .digest('hex')
+  )
+})
+app.listen(process.argv[2])
